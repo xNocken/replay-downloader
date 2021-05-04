a47a7da8a8affdda002f778e2adf15b681e121b3
xnocken <minecraftmarc746@gmail.com>
2021-05-04 12:20:45 +0200

    feat(download): fixes bug with chunk order

--- src/handleDownload.js
+++ src/handleDownload.js
@@ -4,15 +4,15 @@ const handleDownload = async (chunks, deviceAuth, callback, updateCallback) => {
   const downloads = [];
   const results = [];
 
-  chunks.forEach((chunk) => {
+  chunks.forEach((chunk, index) => {
     const download = async () => {
       const data = await downloadFileWithLink(chunk.DownloadLink, chunk.encoding);
 
-      results.push({
+      results[index] = {
         ...chunk,
         size: chunk.size + data.length,
         data: data,
-      });
+      };
 
       updateCallback(chunk.chunkType);
     };

472faf25c1610dcaf487105a90ee43df50480591
xNocken <minecraftmarc746@gmail.com>
2025-02-03 11:51:50 +0100

    fix(client): switch to android game client

--- constants.js
+++ constants.js
@@ -1,8 +1,6 @@
 const constants = {
-  metaDataUrl: 'https://datastorage-public-service-live.ol.epicgames.com/api/v1/data/fnreplaysmetadata/public/',
-  baseDataUrl: 'https://datastorage-public-service-live.ol.epicgames.com/api/v1/access/fnreplays/public/',
-  authClientId: '3446cd72694c4a4485d81b77adbb2141',
-  authClientSecret: '9209d4a5e25a457fb9b07489d313b41a',
+  authClientId: '3f69e56c7649492c8cc29f1af08a8a12',
+  authClientSecret: 'b51ee9cb12234f50a69efa67ef53812e',
   tokenEndpoint: 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token',
   verifyEndpoint: 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/verify',
   timeUntilNextCheck: 120,

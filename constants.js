fea02b4acd4f91ce903b051a8aac527dd90bdfe7
xNocken <minecraftmarc746@gmail.com>
2025-02-03 11:54:15 +0100

    fix(constants): maybe dont delete required stuff :D

--- constants.js
+++ constants.js
@@ -1,4 +1,6 @@
 const constants = {
+  metaDataUrl: 'https://datastorage-public-service-live.ol.epicgames.com/api/v1/data/fnreplaysmetadata/public/',
+  baseDataUrl: 'https://datastorage-public-service-live.ol.epicgames.com/api/v1/access/fnreplays/public/',
   authClientId: '3f69e56c7649492c8cc29f1af08a8a12',
   authClientSecret: 'b51ee9cb12234f50a69efa67ef53812e',
   tokenEndpoint: 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token',
